76026de560f988cd8fd6104795abdc477253f708
Joselow <joshepmanuello@gmail.com>
2026-05-02 20:45:10 -0500

    fix(dates): show date in local time

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,8 +1,8 @@
 import dayjs from "dayjs";
-import utc from 'dayjs/plugin/utc';
+// import utc from 'dayjs/plugin/utc';
 import 'dayjs/locale/es'
 dayjs.locale('es')
-dayjs.extend(utc);
+// dayjs.extend(utc);
 
 export const currentDate = () => {
     const today = dayjs();
@@ -21,7 +21,7 @@ export const formatDate = (date: string) => {
 }
 
 export const formatOnlyHours = (date: string) => {
-    const formattedDate =  dayjs.utc(date).format('h:mm A');
+    const formattedDate =  dayjs(date).format('h:mm A');
     return  formattedDate
 }
 
